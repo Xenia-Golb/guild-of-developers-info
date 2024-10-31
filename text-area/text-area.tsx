4b5b66c3bb62081f97ad8ea2b9e20033d1345f3e
Xenia Golbraykh <karzerin@gmail.com>
2024-10-31 14:29:46 +0300

    [+] add handleChange ui/text-area

--- text-area/text-area.tsx
+++ text-area/text-area.tsx
@@ -1,29 +1,63 @@
-import { HTMLProps, PropsWithChildren } from 'react';
+import { HTMLProps, PropsWithChildren, useEffect, useState } from 'react';
 import clsx from 'clsx';
 
-import s from './text-area/text-area.module.scss';
+import s from './text-area.module.scss';
 
 type TextAreaProps = PropsWithChildren<HTMLProps<HTMLTextAreaElement>> & {
-    variant: 'normal ' | 'error' | 'filled'
+    variant?: 'normal' | 'error' | 'active';
     error?: boolean;
+    label?: string;
     helperText?: string;
+    maxLength?: number;
 };
 export const TextArea = ({
-    variant = 'default',
+    variant = 'normal',
     error = false,
     helperText,
     children,
+    maxLength = 100,
     ...rest
 }: TextAreaProps) => {
-    return (
-        <div className={clsx(s['.text-area-container'], { [s.error]: error })}>
-            <textarea className={clsx(s['text-area'], s[`text-area__variant__${variant}`])} {...rest}>
-                {children}
-            </textarea>
-            <span class={s['character-count']}>0/100</span>
-            {helperText && <p className={clsx(s['helper-text'], { [s['error-text']]: error })}>{helperText}</p>}
-        </div>
-    );
-}
+    const [charCount, setCharCount] = useState(0);
+    const [value, setValue] = useState<string>('');
+
+    const handleChange = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
+        const newValue = event.target.value;
+
+        if (newValue.length <= maxLength) {
+            setValue(newValue);
+            setCharCount(newValue.length);
+        }
+        if (rest.onChange) {
+            rest.onChange(event);
+        }
+    };
+
+    useEffect(() => {
+        if (rest.value) {
+            setValue(rest.value as string);
+            setCharCount((rest.value as string).length);
+        }
+    }, [rest.value]);
+    {
+        return (
+            <div className={clsx(s['text-area-container'], { [s.error]: error })}>
+                <textarea
+                    className={clsx(s['text-area'], s[`text-area__variant__${variant}`])}
+                    onChange={handleChange}
+                    maxLength={maxLength}
+                    value={value}
+                    {...rest}>
+                    {children}
+                </textarea>
+                <span className={s['character-count']}>
+                    {' '}
+                    {charCount}/{maxLength}
+                </span>
+                {helperText && <p className={s['error-text']}>{helperText}</p>}
+            </div>
+        );
+    }
+};
 
 export default TextArea;
