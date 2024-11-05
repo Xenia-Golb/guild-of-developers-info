77ecbbc799272bdb5ee455870e41b1e0dc8c0652
Xenia Golbraykh <karzerin@gmail.com>
2024-11-06 00:41:05 +0300

    create radio-checkbox

---
+++ radio-checkbox/radio-checkbox.tsx
@@ -0,0 +1,31 @@
+import { useState } from 'react';
+import s from './radio-checkbox.module.scss';
+
+type RadioCheckBoxProps = {
+  onChange?: (e: React.ChangeEvent<HTMLInputElement>) => void;
+} & React.InputHTMLAttributes<HTMLInputElement>;
+
+export const RadioCheckBox = ({ onChange, ...props }: RadioCheckBoxProps) => {
+  const [isChecked, setIsChecked] = useState<boolean>(true);
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setIsChecked(e.target.checked);
+    if (onChange) {
+      onChange(e);
+    }
+  };
+
+  return (
+    <div className={s['radio-checkbox']}>
+      <input
+        type='radio'
+        className={s['radio-checkbox__hidden']}
+        checked={isChecked}
+        onChange={handleChange}
+        {...props}
+      />
+      <span
+        className={`${s['radio-checkbox__custom']} ${isChecked ? s['radio-checkbox__custom_checked'] : ''}`}></span>
+    </div>
+  );
+};
